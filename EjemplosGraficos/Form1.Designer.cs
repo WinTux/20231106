5b465e232e76c8390127a452d11aa310e04e8f1f
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-11-10 21:30:13 -0400

    Colas

--- EjemplosGraficos/Form1.Designer.cs
+++ EjemplosGraficos/Form1.Designer.cs
@@ -38,9 +38,21 @@
             label2 = new Label();
             label1 = new Label();
             tabPage2 = new TabPage();
+            label3 = new Label();
+            button3 = new Button();
+            txtContenidoPila = new TextBox();
             tabPage3 = new TabPage();
+            label4 = new Label();
+            button4 = new Button();
+            label5 = new Label();
+            label6 = new Label();
+            button5 = new Button();
+            button6 = new Button();
+            label7 = new Label();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
             SuspendLayout();
             // 
             // tabControl1
@@ -51,7 +63,7 @@
             tabControl1.Location = new Point(-1, 2);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(466, 572);
+            tabControl1.Size = new Size(466, 263);
             tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -67,7 +79,7 @@
             tabPage1.Location = new Point(4, 24);
             tabPage1.Name = "tabPage1";
             tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(458, 544);
+            tabPage1.Size = new Size(458, 235);
             tabPage1.TabIndex = 0;
             tabPage1.Text = "Login";
             // 
@@ -136,35 +148,140 @@
             // 
             // tabPage2
             // 
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(button3);
+            tabPage2.Controls.Add(txtContenidoPila);
             tabPage2.Location = new Point(4, 24);
             tabPage2.Name = "tabPage2";
             tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(458, 544);
+            tabPage2.Size = new Size(458, 235);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "Pilas";
             tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(69, 243);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "label3";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(68, 34);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 1;
+            button3.Text = "Mostrar";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // txtContenidoPila
+            // 
+            txtContenidoPila.Location = new Point(63, 94);
+            txtContenidoPila.Multiline = true;
+            txtContenidoPila.Name = "txtContenidoPila";
+            txtContenidoPila.Size = new Size(353, 124);
+            txtContenidoPila.TabIndex = 0;
+            // 
             // tabPage3
             // 
+            tabPage3.Controls.Add(label4);
+            tabPage3.Controls.Add(button4);
             tabPage3.Location = new Point(4, 24);
             tabPage3.Name = "tabPage3";
-            tabPage3.Size = new Size(458, 544);
+            tabPage3.Size = new Size(458, 235);
             tabPage3.TabIndex = 2;
             tabPage3.Text = "Colas";
             tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(64, 100);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 1;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(52, 38);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 0;
+            button4.Text = "Mostrar";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(31, 290);
+            label5.Name = "label5";
+            label5.Size = new Size(166, 15);
+            label5.TabIndex = 1;
+            label5.Text = "Pase por referencia y por valor";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(41, 391);
+            label6.Name = "label6";
+            label6.Size = new Size(0, 15);
+            label6.TabIndex = 2;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(28, 324);
+            button5.Name = "button5";
+            button5.Size = new Size(115, 23);
+            button5.TabIndex = 3;
+            button5.Text = "Pase por valor";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(266, 325);
+            button6.Name = "button6";
+            button6.Size = new Size(136, 23);
+            button6.TabIndex = 4;
+            button6.Text = "Pase por referencia";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(269, 413);
+            label7.Name = "label7";
+            label7.Size = new Size(0, 15);
+            label7.TabIndex = 5;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(465, 644);
+            Controls.Add(label7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(label6);
+            Controls.Add(label5);
             Controls.Add(tabControl1);
             Name = "Form1";
             Text = "Super ejemplos";
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
             tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -180,5 +297,15 @@
         private TextBox txtUsuario;
         private Button button2;
         private Label lblMensaje;
+        private Button button3;
+        private TextBox txtContenidoPila;
+        private Label label3;
+        private Button button4;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button5;
+        private Button button6;
+        private Label label7;
     }
 }
--- EjemplosGraficos/Form1.cs
+++ EjemplosGraficos/Form1.cs
@@ -37,9 +37,121 @@ namespace EjemplosGraficos
 
         }
 
-        private void Pilas() {
+        private void Pilas()
+        {
             Stack<string> miPila = new Stack<string>();
             //Métodos propios de la pila (en C#)
+            int cant = miPila.Count;
+            miPila.Push("Ana");
+            miPila.Push("Pepe");
+            miPila.Push("Samantha");
+            Console.WriteLine(miPila);
+            txtContenidoPila.Text = miPila.ToString();
+            Stack<string> aux = new Stack<string>();
+            while (miPila.Count > 0)
+            {
+
+                string elemento = miPila.Pop();
+                label3.Text += "\n" + elemento;
+                aux.Push(elemento);
+            }
+            while (aux.Count > 0)
+            {
+                miPila.Push(aux.Pop());
+
+            }
+            label3.Text += "\nElemento de cima: " + miPila.Peek();
+            label3.Text += "\nCantidad de elementos: " + miPila.Count;
+
+        }
+        private void Colas()
+        {
+            Queue<string> colaNombres = new Queue<string>();
+            //Métodos propios de la pila (en C#)
+            int cant = colaNombres.Count;
+            colaNombres.Enqueue("Ana");
+            colaNombres.Enqueue("Pepe");
+            colaNombres.Enqueue("Samantha");
+            Console.WriteLine(colaNombres);
+            label4.Text = colaNombres.ToString();
+            Queue<string> aux = new Queue<string>();
+            while (colaNombres.Count > 0)
+            {
+
+                string elemento = colaNombres.Dequeue();
+                label4.Text += "\n" + elemento;
+                aux.Enqueue(elemento);
+            }
+            while (aux.Count > 0)
+            {
+                colaNombres.Enqueue(aux.Dequeue());
+
+            }
+            label4.Text += "\nElemento delante: " + colaNombres.Peek();
+            label4.Text += "\nCantidad de elementos: " + colaNombres.Count;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Pilas();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Colas();
+        }
+
+        public void EjemploPaseReferenciaValor()
+        {
+            int edad = 45;
+            manipuladorDeNumeros(edad);
+            label6.Text = edad + "";
+        }
+
+        private void manipuladorDeNumeros(int edad)
+        {
+            edad = 10;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            EjemploPaseReferenciaValor();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            EjemploPorReferencia();
+        }
+
+        private void EjemploPorReferencia()
+        {
+            Persona per = new Persona()
+            {
+                edad = 21,
+                nombre = "Pepe",
+                estatura = 1.7
+            };
+            //referencia
+            Persona per_aux = per;
+            per.nombre = "";
+            per.edad = 80;
+            manipuladorDeObjetoPersona(per);
+            label7.Text = string.Format("{0} tiene {1} años con estatura {2}",per.nombre,per.edad,per.estatura);
+        }
+
+        private void manipuladorDeObjetoPersona(Persona p)
+        {
+            p.edad = 90;
+            p.nombre = "Samantha";
+            p.estatura = 1.63;
         }
     }
-}   
+
+    public class Persona
+    {
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public double estatura { get; set; }
+
+    }
+}
